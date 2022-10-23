1ff39665d5fe0dc67c0c218b00e3c3030c38683b
Enes <sevket.enes95@gmail.com>
2022-10-23 18:08:59 +0300

    test commit

--- odevClass/Models/Urun.cs
+++ odevClass/Models/Urun.cs
@@ -5,7 +5,7 @@
         public int Id  { get; set; }
         public string ıtemAdı { get; set; }
         public int ıtemFıyatı { get; set; }
-        public int ıtemAdetı { get; set; }
+        public int ıtemAdetı2 { get; set; }
         public int ıtemSeviyesi { get; set; }
         public string paraBirimi { get; set; }
         public string ıtemİsareti { get; set; }
