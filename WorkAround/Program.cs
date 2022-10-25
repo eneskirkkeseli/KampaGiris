555240821f33c9e3fc8afe76de0735a9973e8b0b
FATIH\ahmet <ahmet.fatih.kucuk@gmail.com>
2022-10-25 12:10:00 +0300

    DÜZENLENDİ

---
+++ WorkAround/Program.cs
@@ -0,0 +1,29 @@
+﻿// See https://aka.ms/new-console-template for more information
+//Console.WriteLine("Hello, World!");
+var toplamTestResult = ToplamUnitTest();
+
+
+int Toplama(int a, int b)
+{
+    var toplam = a + b;
+    return toplam;
+}
+bool ToplamUnitTest()
+{
+    var toplamSonuc = Toplama(1, 2);
+    
+    if (toplamSonuc == 3)
+    {
+        Console.WriteLine("Toplama testi başarılı.");
+        return true;
+    }
+    else
+    {
+        Console.WriteLine("Toplama testi başarısız.");
+        return false;
+    }
+}
+
+
+
+
