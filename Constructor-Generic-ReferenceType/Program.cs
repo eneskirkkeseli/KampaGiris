598a362fc7c39468edd5855636ed94f9bd3f5fdc
Enes KIRKKESELİ <sevket.enes95@gmail.com>
2022-11-02 22:21:51 +0300

    Changes and Add

--- Constructor-Generic-ReferenceType/Program.cs
+++ Constructor-Generic-ReferenceType/Program.cs
@@ -6,12 +6,40 @@ namespace Constructor_Generic_ReferenceType
     {
         static void Main(string[] args)
         {
-            //REFERENCE TYPE:
             //CONSTRUCTOR: (KURUCU METOTLAR)
-            //GENERİC:
+            //Bir classı ilk kez çalıştırdığımız blocktur. Biz classı ise ilk defa new ile çalıştırırız. Ve biz bir sınıfı newlediğimizde çalışmasını istediğimiz codelar varsa constructor yapısını kullanır ve ekleriz.
+            Customer customer = new Customer(25,"Enes","KIRKKESELİ","Erzurum"); //Parametre girilmesi gerekirse aşağıda olduğu gibi belirtilen parametreler girilmelidir. Bunun nedeni aşağıda bulunan constructorda Id, FirstName, LastName ve Cityi belirttiğimiz için otomatik olarak parantez içinde bu verilerin girmesini isteyecek.
+
+            Customer customer1 = new Customer { City = "Erzurum", Id = 25, FirstName = "Enes", LastName = "KIRKKESELİ" }; //Burada belirttiğimiz durum alt alta yazılımın kısa halidir ve bu şekilde de yazılabilir.
+            customer1.FirstName = "Enes";
+            customer1.LastName = "KIRKKESELİ"; //Bu kullanımın kısaltılmış hali yukarıdaki gibidir.
 
 
+            Customer customer2 = new Customer(); //Bu methodun çalışma nedeni ise aşağıda parametresiz bir constructor eklediğimiz için. Eğer eklenmeseydi parametresiz bir constructor özelliklerini belirttiğimiz kurucu, default const u ezeceği için bu bölüm kızacaktı ve çalışmayacaktı boş bir const atana kadar.
+
+
+            Console.WriteLine(customer1.Id); //Artık aşağıda parametre tanımlandığı için const ta burayı ekrana yazdırabiliriz.
+        }
+    }
+
+    class Customer
+    {
+        public Customer(int id, string firstName, string lastName, string city) //void veya başka bir şey dönmüyor. Bu yapı Constructor olur. "ctor" + tab + tab ile oluşur. Normalde yukarıda tanımını yaptığımız customer classı bu constructor metodu olmasa da default constructor olarak arka planda çalışır. Biz burada sadece özel olarak çalışmasını isteyeceğimiz constructor metodunu ekledik. Bu block Customer classının newlenmediği sürece çalışmaz çünkü ona bağlıdır. Dolayısıyla newleyerek ilk classı tanımlarız. Class tanımlandığı için otomatik olarak da bu block çalışır. Ayrıca constructorlara parametreler ekleyebiliriz. Bu class parametrelerini aşağıda atamamızın nedeni Console.Writeline da özel olarak yazdırmak istediğimiz verileri çağırabilmek için.
+        {
+            Id = id;
+            FirstName = firstName;
+            LastName = lastName;
+            City = city;
+        }
+
+        public Customer() //Parametresi olmayan costructor = default constructor denir. Burası kalkarsa yukarıdaki parametreler kızar.
+        {
 
         }
+        
+        public int Id{ get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string City { get; set; }
     }
 }
---
+++ DegerVeReferansTipler/Referance.cs
@@ -0,0 +1,62 @@
+﻿using System;
+
+namespace DegerVeReferansTipler
+{
+    class Referance
+    {
+        static void Main(string[] args)
+        {
+            int sayi1 = 10;
+            int sayi2 = 30;
+            sayi1 = sayi2;
+            sayi2 = 65;
+            Console.WriteLine(sayi1);
+
+            int[] sayilar1 = new int[] { 10, 20, 30 };
+            int[] sayilar2 = new int[] { 100, 200, 300 };
+            sayilar1 = sayilar2;
+            sayilar2[0] = 999;
+            Console.WriteLine(sayilar1[0]);
+
+            //int, float, double, string, bool = değer tipi = steak te depolanır
+            //class, array[dizi], interface = referans tipi = heap de depolanır.
+
+            Person Person1 = new Person();
+            Person1.Id = 1;
+            Person1.FirstName = "Enes";
+            Person1.LastName = "KIRKKESELİ";
+
+            Customer customer = new Customer();
+            customer.CreditCardNumber = "1234";
+
+            Employee employee = new Employee();
+            employee.EmployeeNumber = 5;
+
+            Person Person2 = new Customer();
+            customer.FirstName = "Ali";
+
+
+
+            Console.WriteLine(((Customer)Person2).FirstName); //Mantığını tekrar sor ve neden çalışmadığına bak...
+
+
+
+        }
+    }
+    class Person //Person classına Parent class diyelim. Altına da child classlar ekleyelim. Mantıken child classlar kalıtımı Parent classtan alacak. Dolayısıyla Parent classta hangi durumlar varsa aynıları child için de geçerli olacak. Parent class Person, ona bağlı olan Child classlar ise Employee ve Customer olacak. ÖNEMLİ NOT: Child classlarda yaptığımız değişiklikler personda görülemez. O classa özeldir sadece.
+    {
+        public int Id { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+    }
+
+    class Customer:Person
+    {
+        public string CreditCardNumber { get; set; }
+    }
+
+    class Employee:Person
+    {
+        public int EmployeeNumber { get; set; }
+    }
+}
